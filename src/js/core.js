5806ca9f5a35fa085991ef9b58ca5b8825558a20
SamStalschus <samuel5stalschus@gmail.com>
2021-09-28 21:54:08 -0300

    Feat: Add - Core for calculate

--- src/js/core.js
+++ src/js/core.js
@@ -1,19 +1,72 @@
-var reportedValue = document.getElementById('reportedValue')
-var confirmButton = document.getElementById('submit')
+let reportedValue = document.getElementById('reportedValue')
+let divResult = document.getElementById('divResult')
+let textValues = document.getElementById('textValues')
+let resultNumeric = document.getElementById('resultNumeric')
+let result = document.getElementById('divResult')
+let alert = document.getElementById('alert')
 
-confirmButton.disabled = true
+setHidden(divResult, alert)
 
-function calculate() {
 
+let findPermutations = (string) => {
+  if (!string || typeof string !== "string") {
+    return "Please enter a string"
+  } else if (string.length < 2) {
+    return string
+  }
+
+  let permutationsArray = []
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+
+    if (string.indexOf(char) != i)
+      continue
+
+    let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
+
+    for (let permutation of findPermutations(remainingChars)) {
+      permutationsArray.push(char + permutation)
+    }
+  }
+  return permutationsArray
 }
 
-function enableConfirmButton() {
-  if (confirmButton.value.length > 1) confirmButton.disabled = false
-  else confirmButton.disabled = true
+function setVisible(...elements) {
+  for (const element of elements) {
+    if (element.length && element.length > 0)
+      for (let i = 0; i < element.length; i++) { setVisible(element[i]) }
+    else element.style.display = ''
+  }
+}
+
+function setHidden(...elements) {
+  for (const element of elements) {
+    if (element.length && element.length > 0)
+      for (let i = 0; i < element.length; i++) { setHidden(element[i]) }
+    else element.style.display = 'none'
+  }
+}
+
+function verifyValue() {
+  if (reportedValue.value.length > 1) {
+    setVisible(divResult)
+    setHidden(alert)
+
+    var permutationsArray = findPermutations(reportedValue.value)
+    textValues.value = permutationsArray
+    resultNumeric.innerText = `É possível obter ${permutationsArray.length} possibilidades.`
+
+  } else if (reportedValue.value.length === 0) {
+    setHidden(divResult)
+    setHidden(alert)
+  } else {
+    setHidden(divResult)
+    setVisible(alert)
+  }
 }
 
 $(document).ready(function () {
   console.log('ready boys')
-  reportedValue.addEventListener('keyup', enableConfirmButton)
-  confirmButton.addEventListener('click', calculate)
+  reportedValue.addEventListener('keyup', verifyValue)
 })
